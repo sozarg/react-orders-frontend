e1e8bf323b453fa344bec79baa61364deb2bd104
sozarg <mateoterr@gmail.com>
2025-04-25 16:14:22 -0300

    improve layout spacing by adding DashboardLayout styles

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children, onViewChange, currentView }) => {
   return (
     <div className="dashboard">
       <Sidebar onViewChange={onViewChange} currentView={currentView} />
-      <div className="dashboard-content">
+      <div className="content">
         {children}
       </div>
     </div>
