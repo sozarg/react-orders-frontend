344873f4250fdb2850ed3538ea122ecda3bba89d
sozarg <mateoterr@gmail.com>
2025-04-28 11:10:13 -0300

    Remove old components, styles and unused files after project restructuring

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './ErrorMessage.css';
-
-const ErrorMessage = ({ message }) => {
-  if (!message) return null;
-  return <div className="error-message">{message}</div>;
-};
-
-export default ErrorMessage;
